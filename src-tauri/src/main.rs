3efbb702eccf8f01d369b6ccdac49298485ef005
plawinskig <plawinskig@gmail.com>
2026-08-09 13:16:11 +0200

    Refactor main function to integrate Tauri plugin and workspace handlers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod workspace;
+
 fn main() {
-    accord_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            workspace::get_workspace,
+            workspace::set_workspace
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
