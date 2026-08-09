7b7afe2bb32c38e98a19ca1e4d49573bb57d65da
plawinskig <plawinskig@gmail.com>
2026-08-09 23:52:08 +0200

    Refactor main.rs to include database management in AppState and connect to the database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,26 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sqlx::SqlitePool;
+use tokio::sync::Mutex;
+
 mod workspace;
+mod db;
+
+// app memory
+pub struct AppState {
+    pub db: Mutex<Option<SqlitePool>>,
+}
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .manage(AppState {
+            db: Mutex::new(None),
+        })
         .invoke_handler(tauri::generate_handler![
             workspace::get_workspace,
-            workspace::set_workspace
+            workspace::set_workspace,
+            db::connect_to_db
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
