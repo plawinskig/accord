dae522ac2d1226df3112cf47f4ff05bfe42b5ba3
plawinskig <plawinskig@gmail.com>
2026-08-17 21:58:26 +0200

    save the path so the attachments module can access it

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -36,6 +36,8 @@ pub async fn connect_to_db(workspace_path: String, state: State<'_, AppState>) -
     
     // save the connection pool in the application's memory (state) so that other functions can access it
     *state.db.lock().await = Some(pool);
+    // save the path so the attachments module can access it
+    *state.workspace_path.lock().await = Some(workspace_path);
     
     Ok(())
 }
